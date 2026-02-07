b44403dffc727274380b5f48a7e0591256b3a6f4
diyar <diyardireki111@gmail.com>
2026-02-07 08:33:50 +0300

    t

--- src/features/login/Login.jsx
+++ src/features/login/Login.jsx
@@ -1,5 +1,21 @@
+import Button from "../../components/buttons/Button";
+import Input from "../../components/inputs/Input";
+import "./login.css";
+
 const Login = () => {
-  return <form></form>;
+  return (
+    <section className="container center login-page">
+      <form className="login-form">
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+          dolorem.
+        </h3>
+        <Input label="username" placeholder="enter username" />
+        <Input label="password" placeholder="enter password" />
+        <Button> submit </Button>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
